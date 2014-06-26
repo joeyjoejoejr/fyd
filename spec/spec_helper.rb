dd2b05366e98840b9d31a2d4ad6a59a7711b8d5d
Joe Jackson <cpmhjoe@gmail.com>
2014-06-26 13:50:31 -0400

    Initial Commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  # Print the 10 slowest examples and example groups at the end of the run
+  config.profile_examples = 10
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
